206fae1c5becd67949466ed477f46bdb7dfe3998
Axmat Uyanaev <uyanaev@mego.travel>
2024-09-24 03:18:16 +0300

    fdsfd

--- DiceApi/Startup.cs
+++ DiceApi/Startup.cs
@@ -146,9 +146,13 @@ namespace DiceApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime appLifetime)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiceApi v1"));
+            if (_env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiceApi v1"));
+            }
 
             app.UseRouting();
             app.UseCors("AllowAll");
